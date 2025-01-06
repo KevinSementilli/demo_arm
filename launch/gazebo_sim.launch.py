c1130b0f835edb258af0856518c8c59ea0058bc1
KevinSementilli <ksementilli@hotmail.ca>
2025-01-06 13:42:08 -0500

    velocity controller configured

--- launch/gazebo_sim.launch.py
+++ launch/gazebo_sim.launch.py
@@ -12,59 +12,65 @@ def generate_launch_description():
 
     package_name='demo_arm'
 
+    config_file = os.path.join(get_package_share_directory(package_name), 'config', 'vel_controller.yaml')
+
     # launch robot_state_publisher with sim_time set to true
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+        PythonLaunchDescriptionSource(
+            [os.path.join(get_package_share_directory(package_name),'launch','rsp.launch.py')]), 
+            launch_arguments={'use_sim_time': 'true'}.items()
     )
 
     # launch the gazebo launch file provided with the gazebo_ros package
     gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+        PythonLaunchDescriptionSource(
+            [os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
     )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='gazebo_ros', 
-                        executable='spawn_entity.py',
-                        arguments=['-topic',
-                                   'robot_description',
-                                   '-entity',
-                                   'my_bot'],
-                        output='screen'
-    )
-
-    # launch the joy_node that outputs gamepad interpretation into the /joy topic
-    joy_node_spawner = Node(
-        package='joy',
-        executable='joy_node',
-        name='joy_node',
-        output='screen',
-    )
-
-    # launch the custom node to convert gamepad interpretation into velocity commands
-    gamepad_node = Node(
-        package='robot_teleop',
-        executable='teleop_jointjog_joy',
-        name='teleop_jointjog_joy',
-        output='screen',
-        parameters=[os.path.join(get_package_share_directory, 'config', 'joy_config.yaml')]
+    spawn_entity = Node(
+        package='gazebo_ros', 
+        executable='spawn_entity.py',
+        arguments=['-topic', 'robot_description', '-entity', 'robotic_arm'],
+        output='screen'
     )
 
     # spawn the velocity controller
-    vel_controller_spawner = Node(
+    joint_vel_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["joint_vel_cont"],
     )
 
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
+
+    # launch the joy_node that outputs gamepad interpretation into the /joy topic
+    # joy_node_spawner = Node(
+    #     package='joy',
+    #     executable='joy_node',
+    #     name='joy_node',
+    #     output='screen',
+    # )
+
+    # # launch the custom node to convert gamepad interpretation into velocity commands
+    # gamepad_node = Node(
+    #     package='robot_teleop',
+    #     executable='teleop_jointjog_joy',
+    #     name='teleop_jointjog_joy',
+    #     output='screen',
+    #     parameters=[os.path.join(get_package_share_directory(package_name), 'config', 'joy_config.yaml')],
+    # )
+
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
-        vel_controller_spawner,
-        joy_node_spawner,
-        gamepad_node
+        joint_vel_spawner,
+        joint_broad_spawner,
+        # joy_node_spawner,
+        # gamepad_node,
     ])
-
